ccd9300e80c2a63732861e6f387d4e2aa6bfc19e
Nathan James <nathanjms@outlook.com>
2021-03-27 14:46:48 +0000

    Insert methoid etc

--- src/server.js
+++ src/server.js
@@ -1,23 +1,60 @@
 import express from "express";
-import Movies from "../movies";
+import { MongoClient } from "mongodb";
 
 const app = express();
 app.use(express.json());
 
-const movies = Movies.movies;
+app.get("/api/movies", async (req, res) => {
+  const client = await MongoClient.connect("mongodb://localhost:27017", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
 
-app.get("/api/movies", (req, res) => {
+  const db = client.db("website");
+  const movies = await db.collection("movies").find({}).toArray();
+
   res.status(200).json(movies);
+  client.close();
 });
 
-app.get("/api/movies/(:movieId)", (req, res) => {
+app.get("/api/movies/(:movieId)", async (req, res) => {
+  const client = await MongoClient.connect("mongodb://localhost:27017", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  const db = client.db("website");
   const { movieId } = req.params;
-  const movie = movies.find((movie) => movie.id == movieId);
+  const movie = await db.collection("movies").findOne({ id: movieId });
   if (movie) {
     res.status(200).json(movie);
   } else {
     res.status(404).json("Could not find the movie");
   }
+  client.close();
+});
+
+app.post("/api/movies/add", async (req, res) => {
+  const client = await MongoClient.connect("mongodb://localhost:27017", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  const db = client.db("website");
+  try {
+    const result = await db.collection("movies").insertOne({
+      title: "NathanJms",
+      rank: "101",
+      id: "tt0239848230548",
+      seen: 0,
+    });
+    if (result) {
+      res.status(200).json(result);
+    } else {
+      res.status(404).json("Could not create the movie");
+    }
+  } catch (error) {
+    res.status(404).json(`${error.message}`);
+  }
+  client.close();
 });
 
 app.listen(8000, () => {
