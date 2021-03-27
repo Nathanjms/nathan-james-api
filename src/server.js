8cdf8b4076d411c92b1f2e943811dc3496d7afa0
Nathan James <nathanjms@outlook.com>
2021-03-27 12:43:47 +0000

    Basic get and post requests

--- src/server.js
+++ src/server.js
@@ -1,12 +1,22 @@
 import express from "express";
 
 const app = express();
+app.use(express.json());
 
 app.get("/morning", (req, res) => {
   res.send("Good Morning");
 });
+
 app.get("/afternoon", (req, res) => {
   res.send("Good Afternoon");
+});
+
+app.get("/hello/(:name)", (req, res) => {
+  res.send(`Hello ${req.params.name}`);
+});
+
+app.post("/hello", (req, res) => {
+  res.send(`Hello ${req.body.name}`);
 });
 
 app.listen(8000, () => {
