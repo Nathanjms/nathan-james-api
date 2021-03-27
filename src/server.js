53c8fe0072694e3a83d1daea6fb73cfa2501d2eb
Nathan James <nathanjms@outlook.com>
2021-03-27 18:12:51 +0000

    Add cors package for cross-site requests

--- src/server.js
+++ src/server.js
@@ -1,7 +1,9 @@
 import express from "express";
 import { MongoClient } from "mongodb";
+import cors from "cors";
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.get("/api/movies", async (req, res) => {
