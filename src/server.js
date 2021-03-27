bf6eac1bc1f1496e1d656b860221cb362776b1c7
Nathan James <nathanjms@outlook.com>
2021-03-27 15:00:45 +0000

    Insert movie

--- src/server.js
+++ src/server.js
@@ -40,6 +40,28 @@ app.post("/api/movies/add", async (req, res) => {
   });
   const db = client.db("website");
   try {
+    const newMovie = req.body;
+    console.log(newMovie);
+    const result = await db.collection("movies").insertOne(newMovie);
+    console.log(result);
+    if (result) {
+      res.status(200).json(result);
+    } else {
+      res.status(404).json("Could not create the movie");
+    }
+  } catch (error) {
+    res.status(404).json(`${error.message}`);
+  }
+  client.close();
+});
+
+app.post("/api/movies/mark-seen", async (req, res) => {
+  const client = await MongoClient.connect("mongodb://localhost:27017", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  const db = client.db("website");
+  try {
     const result = await db.collection("movies").insertOne({
       title: "NathanJms",
       rank: "101",
